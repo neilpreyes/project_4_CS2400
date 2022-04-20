c50deef734d39077917183bab968a3b472c0baa0
neilpreyes <98624021+neilpreyes@users.noreply.github.com>
2022-04-19 17:17:58 -0700

    Updated base files
    Created an interface file for heap

--- heap.java
+++ heap.java
@@ -2,6 +2,27 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class heap {
-    
+public class heap implements heapinterface<T>{
+
+    public void add(int newEntry){
+
+    }
+    public int removeMax(){
+
+    }
+    public int getMax(){
+
+    }
+    public boolean isEmpty(){
+
+    }
+    public int getSize(){
+
+    }
+    public void clear(){
+
+    }
+    public void heapify(int rootIndex){
+        
+    }
 }
---
+++ heapinterface.java
@@ -0,0 +1,11 @@
+public interface heapinterface {
+    
+    public void add(int newEntry);
+    public int removeMax();
+    public int getMax();
+    public boolean isEmpty();
+    public int getSize();
+    public void clear();
+    public void heapify(int rootIndex);
+
+}
